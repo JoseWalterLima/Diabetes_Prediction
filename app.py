86094eafff33866027eb04e74fcd798d6e849081
José Walter Lima <60626579+JoseWalterLima@users.noreply.github.com>
2021-01-05 15:32:20 -0300

    Updating Local Host

--- app.py
+++ app.py
@@ -38,6 +38,5 @@ def saida():
     return render_template('resultado.html', previsao=diagnostico)
 
 if __name__ == "__main__":
-    app.debug=True
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
